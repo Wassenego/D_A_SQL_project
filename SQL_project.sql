805f8996f571951ae9785c1868b9b88013471a5f
Marek Sýkora <136271452+Wassenego@users.noreply.github.com>
2024-01-02 12:56:39 +0100

    New version of 4th query

--- SQL_project.sql
+++ SQL_project.sql
@@ -314,6 +314,46 @@ GROUP BY `year`,
 ORDER BY avg_rate_diff DESC
 ;
 
+-- 4.dotaz - 	Existuje rok, ve kterém byl meziroční nárůst cen potravin výrazně vyšší než růst mezd (větší než 10 %)?
+-- 		Verze, kdy porovnáváme průměrný meziroční nárůst cen všech potraviny s průměrným meziročním nárůstem platu všech odvětví dohromady
+SELECT tms.current_year AS `year`,
+	tms.food_growth_rate,
+	ROUND(AVG(tms.salary_growth_rate), 2) AS avg_salary_growth_rate,
+	ROUND(AVG(tms.rate_diff), 2) AS avg_rate_diff
+FROM (
+	SELECT ms1.food_name,
+		ms1.`year` AS previous_year,
+		ms1.average_price AS avg_price_previous,
+		ms2.`year` AS current_year,
+		ms2.average_price AS avg_price_current,
+		ROUND(ms2.average_price / ms1.average_price * 100 - 100, 2) AS food_growth_rate,
+		ms1.industry_branch,
+		ms1.average_salary AS avg_salary_previous,
+		ms2.average_salary AS avg_salary_current,
+		ROUND(ms2.average_salary / ms1.average_salary * 100 - 100, 2) AS salary_growth_rate,
+		ROUND(ms2.average_price / ms1.average_price * 100 - ms2.average_salary / ms1.average_salary * 100, 2) AS rate_diff
+	FROM t_Marek_Sykora_project_SQL_primary_final ms1
+	JOIN (
+		SELECT ms.`year`,
+			ms.food_name,
+			ms.average_price,
+			ms.industry_branch,
+			ms.average_salary 
+		FROM t_Marek_Sykora_project_SQL_primary_final ms
+		ORDER BY ms.food_name, ms.`year` 
+	) ms2 ON ms1.`year` = ms2.`year` - 1 
+		AND ms1.food_name = ms2.food_name 
+		AND ms1.industry_branch = ms2.industry_branch
+	GROUP BY ms1.`year`, 
+		ms1.industry_branch 
+	ORDER BY ms1.food_name, ms1.`year`
+	) tms
+WHERE tms.rate_diff >= 10
+GROUP BY `year`, 
+	tms.food_growth_rate
+ORDER BY avg_rate_diff DESC
+;
+
 -- 5.dotaz - 	Má výška HDP vliv na změny ve mzdách a cenách potravin? Neboli, pokud HDP vzroste výrazněji v jednom roce, projeví se to na cenách potravin či mzdách ve stejném nebo následujícím roce výraznějším růstem?
 SELECT tms.current_year AS `year`,
 	tms2.country,
