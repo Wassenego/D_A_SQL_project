defb71f2d5cfb2cf1118176b2a3eadf90b46c473
Marek Sýkora <mareksykora78@seznam.cz>
2024-02-07 22:27:34 +0100

    Final correction of Question_2

--- SQL_project_question_1.sql
+++ SQL_project_question_1.sql
@@ -4,7 +4,8 @@ Discord display name: Marek S.*/
 
 -- 1.dotaz - 	Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 -- 		Varianta s výpisem všech kombinací a se slovním hodnocením  	    	
-SELECT ms.industry_branch,
+SELECT 
+	ms.industry_branch,
 	ms1.`year` AS `year`,
 	ms.average_salary AS previous_year_salary,
 	'Kč' AS unit,
@@ -18,14 +19,18 @@ FROM t_marek_sykora_project_sql_primary_final ms
 JOIN t_marek_sykora_project_sql_primary_final ms1 
 	ON (ms.`year` + 1) = ms1.`year` 
 	AND ms.industry_code = ms1.industry_code
-GROUP BY ms.industry_branch, ms.`year` 
-ORDER BY ms1.industry_code, 
+GROUP BY 
+	ms.industry_branch, 
+	ms.`year` 
+ORDER BY 
+	ms1.industry_code, 
 	ms1.`year`
 ;
 
 -- 1.dotaz - 	Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 -- 		Výpis pouze odvětví a roků, kdy mzdy klesly
-SELECT ms.industry_branch,
+SELECT 
+	ms.industry_branch,
 	ms.industry_code,
 	ms1.`year` AS `year`,
 	CASE 
@@ -36,20 +41,24 @@ FROM t_marek_sykora_project_sql_primary_final ms
 JOIN t_marek_sykora_project_sql_primary_final ms1 
 	ON (ms.`year` + 1) = ms1.`year` 
 	AND ms.industry_code = ms1.industry_code
-GROUP BY ms.industry_branch, ms.`year` 
+GROUP BY 
+	ms.industry_branch, 
+	ms.`year` 
 HAVING salary_evaluation != 1
 ORDER BY `year`
 ;
 
 -- 1.dotaz - 	Rostou v průběhu let mzdy ve všech odvětvích, nebo v některých klesají?
 -- 		Varianta s výpisem všech kombinací a se slovním hodnocením - alternativní verze 	    	
-SELECT ms2.industry_branch,
+SELECT 
+	ms2.industry_branch,
 	ms2.industry_code,
 	ms2.`year` AS `year`
 FROM (
-	SELECT ms.industry_branch,
-	ms.industry_code,
-	ms1.`year` AS `year`,
+	SELECT 
+		ms.industry_branch,
+		ms.industry_code,
+		ms1.`year` AS `year`,
 		CASE 
 			WHEN ms.average_salary < ms1.average_salary THEN 1
 			ELSE 0
@@ -58,7 +67,9 @@ FROM (
 	JOIN t_marek_sykora_project_sql_primary_final ms1 
 		ON (ms.`year` + 1) = ms1.`year` 
 		AND ms.industry_code = ms1.industry_code
-	GROUP BY ms.industry_branch, ms.`year`
+	GROUP BY 
+		ms.industry_branch, 
+		ms.`year`
 ) ms2
 WHERE ms2.salary_evaluation = 0
 ORDER BY ms2.`year`
